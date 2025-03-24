f43ccb9ea1dfa04a27b7261a6e44c1127499f61b
Nardandersen <136600443+Nardandersen@users.noreply.github.com>
2025-03-24 21:49:21 +0800

    models.py

--- car_dealer_portal/models.py
+++ car_dealer_portal/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.core.validators import *
 from django.contrib.auth.models import User
 
-# Create your models here.
+
 
 class Area(models.Model):
     pincode = models.CharField(validators = [MinLengthValidator(6), MaxLengthValidator(6)],max_length = 6,unique=True)
