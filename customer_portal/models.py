1392785eef04399dbf5abff5beeaa2c573e79f29
Nardandersen <136600443+Nardandersen@users.noreply.github.com>
2025-03-24 21:51:51 +0800

    models.py

--- customer_portal/models.py
+++ customer_portal/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.core.validators import *
 from django.contrib.auth.models import User
 from car_dealer_portal.models import *
-# Create your models here.
+
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
